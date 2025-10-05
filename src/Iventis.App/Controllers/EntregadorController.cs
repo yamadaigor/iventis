4e35a5ccf9d90007c54d3383caa808d875e005a8
yamadaigor <yamadaigor@yahoo.com.br>
2025-10-05 11:42:47 -0300

    first commit

---
+++ src/Iventis.App/Controllers/EntregadorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Iventis.App.Controllers
+{
+    [ApiController]
+    public class EntregadorController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/Iventis.App/Controllers/MotoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Iventis.App.Controllers
+{
+    [ApiController]
+    public class MotoController : ControllerBase
+    {
+        // Colocar Filtros para claims.
+
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
